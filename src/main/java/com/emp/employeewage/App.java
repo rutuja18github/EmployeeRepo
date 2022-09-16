8bd0d1bfa48471292169828ec58e161dbe5168b1
RutujaPatil18 <prutuja189@gmail.com>
2022-09-16 15:48:20 +0530

    Initial Commit

---
+++ src/main/java/com/emp/employeewage/App.java
@@ -0,0 +1,11 @@
+package com.emp.employeewage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}		
+}
