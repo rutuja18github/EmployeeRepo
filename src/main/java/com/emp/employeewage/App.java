aa9caec55cd68b15c4812afd7eae8b405cbcbb64
RutujaPatil18 <prutuja189@gmail.com>
2022-09-16 16:01:44 +0530

    UC-2:Employee Daily Wage

--- src/main/java/com/emp/employeewage/App.java
+++ src/main/java/com/emp/employeewage/App.java
@@ -8,6 +8,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		Employee emp=new Employee();
-		emp.EmpAttendance();
+		System.out.println("Employee's One Day Salary is :" + emp.dailyEmpWage());
 	}		
 }
--- src/main/java/com/emp/employeewage/Employee.java
+++ src/main/java/com/emp/employeewage/Employee.java
@@ -3,16 +3,30 @@ package com.emp.employeewage;
 import java.util.Random;
 
 public class Employee {
-	boolean attendence ;
-	Random random = new Random();
+	boolean attendence;
+	int empHrs;
+	int salary;
+	int empRatePerHr = 20;
 	
-	public void EmpAttendance() {
+	Random random = new Random();
+
+	public boolean empAttendance() {
 		attendence = random.nextBoolean();
 		if (attendence) {
-			System.out.println("Employee is Present");	
+			System.out.println("Employee is Present");
 		} else {
 			System.out.println("Employee is Absent");
-	    }
+		}
+		return attendence;
 	}
 
+	public int dailyEmpWage() {
+		if (empAttendance()) {
+			empHrs = 8;
+		} else {
+			empHrs = 0;
+		}
+		salary = empRatePerHr * empHrs;
+		return salary;
+	}
 }
